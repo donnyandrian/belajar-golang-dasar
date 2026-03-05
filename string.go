0cb6076dc62f9638698423bd7a9dd6079eb70fed
xellanix <107340353+xellanix@users.noreply.github.com>
2026-03-05 16:59:03 +0700

    11

--- string.go
+++ string.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("Eko")
 	fmt.Println("Eko Kurniawan")
 	fmt.Println("Eko Kurniawan Khannedy")
+
+	fmt.Println(len("Eko"))
+	fmt.Println("Eko Kurniawan"[0])
+	fmt.Println("Eko Kurniawan Khannedy"[1])
 }
